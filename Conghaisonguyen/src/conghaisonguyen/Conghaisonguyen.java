00b741034d7125a9c5c77f6000f652c848d34ca6
NguyenVanPhuc <nguyenvanphuc23@dtu.edu.vn>
2025-03-13 07:45:49 +0700

    Cong hai so

---
+++ Conghaisonguyen/src/conghaisonguyen/Conghaisonguyen.java
@@ -0,0 +1,13 @@
+
+package conghaisonguyen;
+
+public class Conghaisonguyen {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a =3;
+        int b=4;
+        System.out.println("Cong hai so nguyen to " +(a+b));
+    }
+    
+}
